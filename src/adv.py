8bd3240a708124a3c2a0d361d1499978a8f6310e
Darkwolfxj <hesitation.dotdot@gmil.com>
2020-06-03 23:06:03 -0400

    I'm floored by how well I wrote this code, it took a lot of work but I finally got it where I want it

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,18 @@
 from room import Room
+from player import Player
+from time import sleep
 
 # Declare all the rooms
-
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east."),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
 }
 
 
@@ -27,8 +21,8 @@ earlier adventurers. The only exit is to the south."""),
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
 room['overlook'].s_to = room['foyer']
+room['foyer'].e_to = room['narrow']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
@@ -49,3 +43,37 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+class Adv:
+    def __init__(self, go=None):
+        current_room = room['outside']
+        game_over = False
+        player = Player
+        player(current_room)
+        while game_over is not True:
+            
+            sleep(1)
+            direction = input("Enter a direction in which to move: ")
+        
+            if direction == 'north' or direction == 'North' or direction == 'NORTH':
+                direction = 'n_to'
+            elif direction == 'south' or direction == 'South' or direction == 'SOUTH':
+                direction = 's_to'
+            elif direction == 'west' or direction == 'West' or direction == 'WEST':
+                direction = 'w_to'
+            elif direction == 'east' or direction == 'East' or direction == 'EAST':
+                direction = 'e_to'
+            else:
+                print("That's not a valid direction.")
+            try: 
+                current_room = getattr(current_room, direction)   
+                player(current_room)
+                if current_room == room['treasure']:
+                    sleep(5)
+                    print("Congratulations! You Win!")
+                    game_over = True
+            except:
+                print("Cannot move in that direction right now.")
+                sleep(1)
+                player(current_room)    
+instance = Adv()
--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+    def __repr__(self):
+        return f"You are currently in the {self.name} room. {self.desc}"
